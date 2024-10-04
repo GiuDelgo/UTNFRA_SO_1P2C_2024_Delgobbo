d013728379273796d3ed5ddca82664649b4742c1
GiuDelgo <delgobbogiuliana@gmail.com>
2024-10-04 15:23:03 +0000

    feat: agergo foramteo

--- RTA_SCRIPT_Examen_20241004/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_B.sh
@@ -2,6 +2,9 @@
 
 DISCO=$(sudo fdisk -l |grep "10 GiB" | awk '{print $2}' | awk -F ':' '{print $1}')
 
+echo "Creo particiones de $DISCO"
+echo
+
 sudo fdisk $DISCO << EOF
 n
 e
@@ -41,3 +44,14 @@ n
 
 
 EOF
+echo
+echo "particiones creadas"
+echo
+
+for NRO in {5..14}; do
+   echo "Formateando ${DISCO}${NRO}"
+   sudo mkfs.ext4 "${DISCO}${NRO}"
+done
+echo
+
+
