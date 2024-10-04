225c8f3b4609470278d07abf52b284a873f14785
GiuDelgo <delgobbogiuliana@gmail.com>
2024-10-04 17:03:13 +0000

    feat: agregue montaje persistente y permisos

--- RTA_SCRIPT_Examen_20241004/Punto_B.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_B.sh
@@ -1,4 +1,4 @@
-#!bin/bash
+#!/bin/bash
 
 DISCO=$(sudo fdisk -l |grep "10 GiB" | awk '{print $2}' | awk -F ':' '{print $1}')
 
@@ -43,6 +43,7 @@ n
 n
 
 
+w
 EOF
 echo
 echo "particiones creadas"
@@ -53,5 +54,30 @@ for NRO in {5..14}; do
    sudo mkfs.ext4 "${DISCO}${NRO}"
 done
 echo
+
+FLAG=$(grep -c $DISCO /etc/fstab)
+
+
+if [[ $FLAG == 0 ]]; then
+	echo "monto de forma persistente"
+	
+	echo "${DISCO}5 /Examenes-UTN/alumno_1/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}6 /Examenes-UTN/alumno_1/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}7 /Examenes-UTN/alumno_1/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	
+	echo "${DISCO}8 /Examenes-UTN/alumno_2/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}9 /Examenes-UTN/alumno_2/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}10 /Examenes-UTN/alumno_2/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	
+	echo "${DISCO}11 /Examenes-UTN/alumno_3/parcial_1 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}12 /Examenes-UTN/alumno_3/parcial_2 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	echo "${DISCO}13 /Examenes-UTN/alumno_3/parcial_3 ext4 defaults 0 0" | sudo tee -a /etc/fstab
+	
+	echo "${DISCO}14 /Examenes-UTN/profesores ext4 defaults 0 0" | sudo tee -a /etc/fstab
+fi
+
+sudo mount -a
+
+
 
 
