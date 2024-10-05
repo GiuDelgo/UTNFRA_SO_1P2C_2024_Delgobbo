b9d2d75119fe82f0aa9519657ae4a8fe14a1b52e
GiuDelgo <delgobbogiuliana@gmail.com>
2024-10-05 18:01:42 +0000

    fix: comando dir profesores

--- RTA_SCRIPT_Examen_20241004/Punto_C.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_C.sh
@@ -29,8 +29,8 @@ sudo chown -R p1c2_2024_A3:p1c2_2024_A3 /Examenes-UTN/alumno_3
 sudo chmod -R 700 /Examenes-UTN/alumno_3
 echo "permisos de /Examenes-UTN/alumno_3 cambiados"
 echo
-sudo chown -R p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
-sudo chmod -R 775 /Examenes-UTN/profesores
+sudo chown p1c2_2024_P1:p1c2_2024_gProfesores /Examenes-UTN/profesores
+sudo chmod 775 /Examenes-UTN/profesores
 echo "permisos de /Examenes-UTN/profesores cambiados"
 echo
 
@@ -38,4 +38,4 @@ sudo su -c "whoami > /Examenes-UTN/alumno_1/validar.txt" p1c2_2024_A1
 sudo su -c "whoami > /Examenes-UTN/alumno_2/validar.txt" p1c2_2024_A2
 sudo su -c "whoami > /Examenes-UTN/alumno_3/validar.txt" p1c2_2024_A3
 sudo su -c "whoami > /Examenes-UTN/profesores/validar.txt" p1c2_2024_P1
-echo"archivos de validacion generados"
+echo "archivos de validacion generados"
