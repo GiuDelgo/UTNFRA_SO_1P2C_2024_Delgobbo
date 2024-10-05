055abf754c84a50b7aa1328dc158bf21d97b82b2
GiuDelgo <delgobbogiuliana@gmail.com>
2024-10-05 18:06:17 +0000

    fix: echo mal escrito

--- RTA_SCRIPT_Examen_20241004/Punto_E.sh
+++ RTA_SCRIPT_Examen_20241004/Punto_E.sh
@@ -4,6 +4,8 @@ cat /proc/meminfo | grep MemTotal > /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_De
 
 sudo dmidecode -t chassis | tail -n12 >> /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Delgobbo/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt
 
+echo
 echo "archivo Filtro_Basico.txt generado"
 echo
-echo"ejecutar -----> cat /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Delgobbo/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt "
+echo "ejecutar -----> cat /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Delgobbo/RTA_ARCHIVOS_Examen_20241004/Filtro_Basico.txt "
+echo
