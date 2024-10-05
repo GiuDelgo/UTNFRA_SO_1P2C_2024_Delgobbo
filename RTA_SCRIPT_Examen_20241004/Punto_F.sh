5cf5ee2d3655dd685addb6b725c979b24a0d1fd1
GiuDelgo <delgobbogiuliana@gmail.com>
2024-10-05 17:00:34 +0000

    feat: comandos pto F scripteados

---
+++ RTA_SCRIPT_Examen_20241004/Punto_F.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cat << EOF > /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Delgobbo/RTA_ARCHIVOS_Examen_20241004/Filtro_Avanzado.txt
+
+Mi IP Publica es: $(curl -s ifconfig.me)
+Mi usuario es: $(whoami)
+El Hash de mi Usuario es: $(sudo grep -i vagrant /etc/shadow | awk -F ':' '{print $2}')
+La URL de mi resporio es: $(git remote get-url origin)
+EOF
+
+echo "archivo Filtro_Avanzado.txt generado"
+echo
+echo "ejecutar ----> cat /home/vagrant/repogit/UTNFRA_SO_1P2C_2024_Delgobbo/RTA_ARCHIVOS_Examen_20241004/Filtro_Avanzado.txt"
